df1a8c9860331e8e2c2559c14a095e1c6859e892
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-02 01:41:14 +0000

    Adjust content entry to include tempaltes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./templates/**/*.{html,html.tera}"
+  ],
+
   theme: {
     extend: {},
   },
+
   plugins: [],
 }
 
