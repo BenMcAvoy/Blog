8d1d01d94fae6f93f6c380e88c82b23c10e1d6f5
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-02 02:10:48 +0000

    Server public files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #[macro_use] extern crate rocket;
 
+use rocket::fs::FileServer;
+
 mod templates;
 use templates::*;
 
@@ -8,7 +10,8 @@ use rocket_dyn_templates::Template;
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
         .register("/", catchers![templates::not_found])
+        .mount("/public", FileServer::from("public/"))
+        .mount("/", routes![index])
         .attach(Template::fairing())
 }
