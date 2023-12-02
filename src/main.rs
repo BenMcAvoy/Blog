7328ccde7f2e6144755254cbaf753389d5aeb029
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-02 02:25:44 +0000

    Create a route for getting robots.txt

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 #[macro_use] extern crate rocket;
 
-use rocket::fs::FileServer;
+use rocket::fs::{FileServer, NamedFile};
 
 mod templates;
 use templates::*;
 
 use rocket_dyn_templates::Template;
 
+#[get("/robots.txt")]
+async fn robots() -> Option<NamedFile> {
+    NamedFile::open("public/robots.txt").await.ok()
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .register("/", catchers![templates::not_found])
         .mount("/public", FileServer::from("public/"))
-        .mount("/", routes![index])
+        .mount("/", routes![index, robots])
         .attach(Template::fairing())
 }
