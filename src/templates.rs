47f33cfe6f0775db93c6b8ee8a041c11f3ad00ee
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-03 01:34:07 +0000

    Update `get_post` route to use the render module

--- src/templates.rs
+++ src/templates.rs
@@ -1,14 +1,7 @@
-use pulldown_cmark::{html, Options, Parser};
-
-use rocket_dyn_templates::{
-    context,
-    tera::{Context, Tera},
-    Template,
-};
-
+use rocket_dyn_templates::{context, Template};
 use rocket::*;
 
-use crate::utils::calculate_age;
+use crate::{render::render, utils::calculate_age};
 
 const BIRTHDATE: Option<chrono::NaiveDate> = chrono::NaiveDate::from_ymd_opt(2009, 3, 13);
 
@@ -22,21 +15,14 @@ pub fn index() -> Template {
     )
 }
 
-#[get("/post/<name>")]
+#[get("/posts")]
+pub fn get_posts() -> Template {
+    todo!()
+}
+
+#[get("/posts/<name>")]
 pub fn get_post(name: String) -> Template {
-    let path = format!("./templates/posts/{name}.md");
-    let markdown = std::fs::read_to_string(path).expect("Valid post");
-
-    let mut tera = Tera::default();
-    tera.add_raw_template("post", &markdown).unwrap();
-
-    let context = Context::new();
-    let rendered_markdown = tera.render("post", &context).unwrap();
-
-    let parser = Parser::new_ext(&rendered_markdown, Options::all());
-
-    let mut html = String::new();
-    html::push_html(&mut html, parser);
+    let html = render(name);
 
     Template::render(
         "post",
