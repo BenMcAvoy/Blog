2b207717e7e2f3700b194450db69864b2ad3031c
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-03 20:46:39 +0000

    Create a function to load posts as a vector of posts

--- src/posts.rs
+++ src/posts.rs
@@ -1,11 +1,26 @@
 use crate::render::render;
 
+#[derive(Debug)]
 pub struct Post {
-    content: String,
-    name: String,
-    url: String,
+    pub content: String,
+    pub name: String,
+    pub url: String,
 }
 
-pub fn load_posts(names: Vec<String>) -> Vec<Post> {
-    todo!("Return posts");
+impl Post {
+    pub fn new(content: String, name: String, url: String) -> Post {
+        Post {
+            content,
+            name,
+            url,
+        }
+    }
+}
+
+pub fn load_posts(filenames: Vec<String>) -> Vec<Post> {
+   filenames.into_iter().map(|filename| {
+       let html = render(filename.clone());
+       let path = format!("/posts/{}", filename);
+       Post::new(html, String::from(""), path)
+   }).collect()
 }
