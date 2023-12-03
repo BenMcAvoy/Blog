0f544378d13becbc91ba20e1a376f65b1daed2ec
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-03 23:40:50 +0000

    Get and save summary from documents

--- src/posts.rs
+++ src/posts.rs
@@ -1,17 +1,45 @@
+use std::fs;
+
 use crate::render::render;
 use serde::Serialize;
 
 #[derive(Debug, Serialize)]
 pub struct Post {
     pub content: String,
+    pub summary: String,
     pub name: String,
     pub url: String,
 }
 
 impl Post {
-    pub fn new(content: String, name: String, url: String) -> Post {
-        Post { content, name, url }
+    pub fn new(content: String, summary: String, name: String, url: String) -> Post {
+        Post {
+            content,
+            summary,
+            name,
+            url,
+        }
     }
+}
+
+fn generate_summary(text: String) -> String {
+    // Split the input into lines and take the second line
+    let lines: Vec<&str> = text.lines().collect();
+
+    // TODO: Improve getting line
+    let text = lines[2];
+
+    // Convert the string into a vector of characters
+    let chars: Vec<char> = text.chars().collect();
+
+    // Convert the vector back into a string
+    let result: String = chars.into_iter().collect();
+
+    // Truncate the string to 250 characters
+    let mut truncated_result = result;
+    truncated_result.truncate(100);
+
+    truncated_result + "..."
 }
 
 pub fn load_posts(filenames: Vec<String>) -> Vec<Post> {
@@ -20,6 +48,11 @@ pub fn load_posts(filenames: Vec<String>) -> Vec<Post> {
         .map(|filename| {
             let html = render(filename.clone(), None);
             let path = format!("/posts/{}", filename);
+
+            let filepath = format!("./templates/posts/{filename}.md");
+            dbg!(&filepath);
+            let text = fs::read_to_string(filepath).expect("Valid path");
+            let summary = generate_summary(text);
 
             let name: String = filename
                 .split('-')
@@ -33,7 +66,7 @@ pub fn load_posts(filenames: Vec<String>) -> Vec<Post> {
                 .collect::<Vec<String>>()
                 .join(" ");
 
-            Post::new(html, name, path)
+            Post::new(html, summary, name, path)
         })
         .collect()
 }
