d316cd566783e06b516bfdfcdfadd11cdf02111f
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-03 01:48:08 +0000

    Implement synax highlighting in the render module

--- src/render.rs
+++ src/render.rs
@@ -1,6 +1,9 @@
-use pulldown_cmark::{html, Options, Parser};
+use highlight_pulldown::highlight_with_theme;
+use pulldown_cmark::{html, Parser};
 use rocket_dyn_templates::tera::{Context, Tera};
 use std::{borrow::Cow, fmt::Display};
+
+const THEME: &str = "base16-eighties.dark";
 
 pub fn render<S>(name: S) -> String
 where
@@ -15,10 +18,12 @@ where
     let context = Context::new();
     let rendered_markdown = tera.render("post", &context).unwrap();
 
-    let parser = Parser::new_ext(&rendered_markdown, Options::all());
+    // let parser = Parser::new_ext(&rendered_markdown, Options::all());
+    let events = Parser::new(&rendered_markdown);
+    let events = highlight_with_theme(events, THEME).unwrap();
 
     let mut html = String::new();
-    html::push_html(&mut html, parser);
+    html::push_html(&mut html, events.into_iter());
 
     html
 }
