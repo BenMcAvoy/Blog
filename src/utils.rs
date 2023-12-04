1a05c8e8fa5276f594cadf85bc2e55a5b433dab2
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-12-04 13:28:57 +0000

    Create functions to capitalize and snake to titlecase

--- src/utils.rs
+++ src/utils.rs
@@ -5,3 +5,27 @@ pub fn calculate_age(birthdate: Option<NaiveDate>) -> i32 {
     let today = Utc::now().naive_utc();
     today.year() - birthdate.year()
 }
+
+fn capitalize(word: &&str) -> String {
+    let word = word.to_owned().to_owned();
+
+    match word.chars().next() {
+        Some(first) => {
+            let capitalized_first = first.to_uppercase();
+            let rest = &word[1..].to_lowercase();
+            format!("{}{}", capitalized_first, rest)
+        }
+        None => String::new(),
+    }
+}
+
+pub fn snake_to_titlecase<T>(text: T) -> String
+where
+    T: ToString,
+{
+    let text = text.to_string();
+    let words: Vec<&str> = text.split(' ').collect();
+    let words: Vec<String> = words.iter().map(capitalize).collect();
+
+    words.join(" ")
+}
